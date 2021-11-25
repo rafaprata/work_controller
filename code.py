df7ac1fb35f34d13de453c8baba753533e1acf45
Rafael Prata <rafaelpratasouza@gmail.com>
2021-11-25 15:49:51 -0300

    adding jsons functions

--- code.py
+++ code.py
@@ -33,13 +33,46 @@ def registro_ponto():
     dicio = ponto.cria_dict(keys[0], keys[1], keys[2], keys[3])
     return dicio
 
-def createJson(jfile):
+def createJson(jfile, ponto):
+    ponto_key = list(ponto.keys())[0]
+    jfile[ponto_key] = ponto[ponto_key]
     pontoJson = json.dumps(jfile)
     return pontoJson
 
+def readJsonFile():
+    try:
+        file = open("database.json", "r")
+        try:
+            jsonText = json.load(file)
+            file.close()
+            return jsonText
+        except:
+            jsonText = {}
+            return jsonText
+    except:
+        jsonText = {}
+        return jsonText
+
+def saveJsonFile(db):
+    file = open("database.json", "w")
+    file.write(db)
+    file.close()
+
+def createFile():
+    try:
+        file = open("database.json", "r")
+    except:
+        file = open("database.json", "w")
+    finally:
+        file.close()
+
 def initialize():
+    createFile()
+    jsondb = readJsonFile()
     pontoDict = registro_ponto()
-    print(createJson(pontoDict))
+    pontoJson = createJson(jsondb, pontoDict)
+    saveJsonFile(pontoJson)
+
 
 if __name__ == "__main__":
     initialize()
